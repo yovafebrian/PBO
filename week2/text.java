bb2ec2b627a4fa869ba27e87b584a242de757602
yovafebrian <yova.febrian512@gmail.com>
2024-03-19 16:57:38 +0700

    add week2

---
+++ week2/text.java
@@ -0,0 +1,10 @@
+package week2;
+public class text {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        
+        System.out.println("Original Text : "+str);
+        //converting string to uppercase using method toUpperCase()
+    }
+    
+}
